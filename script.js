8f2faf6132a97a5a626550042439cc977ea23077
Ely Carvalho <elycarvalho3@gmail.com>
2022-11-20 21:11:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const grade = document.querySelector('#grade')
 const cor = document.querySelector('#cor')
-
+const btnLimpa = document.querySelector('#btnLimpa')
+const corFundo = document.querySelector('#corFundo')
 
 function criaGrade() {
-	for (var i = 0; i < 100; i++) {
+	for (var i = 0; i < 289; i++) {
 		grade.innerHTML += `<div class="caixa quadro${i + 1}"></div>`
 	}
 }
 criaGrade()
-grade.addEventListener('click', function(e) {
+
+const caixas = document.querySelectorAll('.caixa')
+
+btnLimpa.addEventListener('click', () => {
+	caixas.forEach(preenche)
+	function preenche(item, index) {
+	  caixas[index].style.backgroundColor = '#fff'
+	}
+})
+
+
+let mouse = 'mouseover'
+grade.addEventListener(mouse, function(e) {
 	if (e.target.classList.contains('caixa')) {
 		e.target.style.backgroundColor = cor.value
 	}
 })
+
+
+function preencheFundo() {
+	caixas.forEach(preencheFundo)
+	function preencheFundo(item, index) {
+	  caixas[index].style.backgroundColor = corFundo.value
+	}
+}
+
+function mudaForma() {
+	caixas.forEach(mudaForma)
+	function mudaForma(item, index) {
+	  caixas[index].style.borderRadius = '3px'
+	}
+}
+
+mudaForma()
