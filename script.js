a8b720395cb0b4f60a3aa76b199c197949fc46a3
Ely Carvalho <elycarvalho3@gmail.com>
2022-11-22 05:55:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const grade = document.querySelector('#grade')
 const cor = document.querySelector('#cor')
 const btnLimpa = document.querySelector('#btnLimpa')
 const corFundo = document.querySelector('#corFundo')
+const cliqueMouse = document.querySelector('#cliqueMouse')
+let mouse = 'mouseover'
 
 function criaGrade() {
-	for (var i = 0; i < 289; i++) {
+	for (var i = 0; i < 340; i++) {
 		grade.innerHTML += `<div class="caixa quadro${i + 1}"></div>`
 	}
 }
 criaGrade()
+mudaMouse()
 
 const caixas = document.querySelectorAll('.caixa')
 
@@ -19,19 +22,50 @@ btnLimpa.addEventListener('click', () => {
 	}
 })
 
+var x = 0
+function mudaMouse() {
+	mudaMouse2('break')
+    grade.addEventListener('click', function(e) {
+	    if (e.target.classList.contains('caixa')) {
+		e.target.style.backgroundColor = cor.value
+	    }
+    })
+}
 
-let mouse = 'mouseover'
-grade.addEventListener(mouse, function(e) {
+function mudaMouse2() {
+    grade.addEventListener('mouseover', function(e) {
+	    if (e.target.classList.contains('caixa')) {
+		e.target.style.backgroundColor = cor.value
+	    }
+    })
+}
+
+	
+	/*if(cliqueMouse.value == 'passar') {mouse = 'mouseover'}
+		console.log('mouse: ' + mouse)
+	grade.addEventListener(mouse)
+	//console.log('cliqueMouse: ' + cliqueMouse.value)*/
+
+
+/*grade.addEventListener('click', function(e) {
 	if (e.target.classList.contains('caixa')) {
 		e.target.style.backgroundColor = cor.value
 	}
-})
+})*/
 
 
 function preencheFundo() {
-	caixas.forEach(preencheFundo)
-	function preencheFundo(item, index) {
+	grade.style.backgroundColor = corFundo.value
+	caixas.forEach(preenche)
+	function preenche(item, index) {
 	  caixas[index].style.backgroundColor = corFundo.value
+	}
+}
+
+function preenche() {
+	caixas.forEach(preenche)
+	function preenche(item, index) {
+	  caixas[index].style.backgroundColor = preencher.value
 	}
 }
 
